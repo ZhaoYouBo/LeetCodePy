07bfb2bcb33ad4f1b28d059085aa20c29e1bf89a
ZhaoRongBo <ouyangyilong427@gmail.com>
2025-07-02 17:31:27 +0800

    Completed Move Zeroe problem

---
+++ code_283_move_zeroes.py
@@ -0,0 +1,14 @@
+"""
+Brief: Move Zeroes problem solution.
+Author: ZhaoRongBo
+Date: 2024-07-02
+Version: 1.0
+"""
+from typing import List
+
+class MoveZeroes:
+    @staticmethod
+    def move_zeroes(nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
