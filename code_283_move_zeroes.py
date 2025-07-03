472921860a445fc1bca14eb34f52612067aa59de
ZhaoRongBo <ouyangyilong427@gmail.com>
2025-07-03 17:07:09 +0800

    Completed Move Zeroe problem

--- code_283_move_zeroes.py
+++ code_283_move_zeroes.py
@@ -1,7 +1,7 @@
 """
 Brief: Move Zeroes problem solution.
 Author: ZhaoRongBo
-Date: 2024-07-02
+Date: 2024-07-03
 Version: 1.0
 """
 from typing import List
@@ -10,5 +10,22 @@ class MoveZeroes:
     @staticmethod
     def move_zeroes(nums: List[int]) -> None:
         """
-        Do not return anything, modify nums in-place instead.
+        Move all zeroes to the end of the list.
+
+        Args:
+            nums (List[int]): The list of integers.
+
+        Returns:
+            None
         """
+        zero_count = nums.count(0)
+        nums[:] = [num for num in nums if num != 0]
+        nums.extend([0] * zero_count)
+
+def test():
+    nums = [0, 1, 0, 3, 12]
+    MoveZeroes.move_zeroes(nums)
+    print(nums)
+
+if __name__ == "__main__":
+    test()
