c7c2da68e91b45d6c9ebaa15819a90378f8403b0
ZhaoRongBo <ouyangyilong427@gmail.com>
2025-07-08 08:27:08 +0800

    Container With Most Water problem

--- code_11_container_with_most_water.py
+++ code_11_container_with_most_water.py
@@ -10,13 +10,13 @@ class Solution:
     @staticmethod
     def max_area(height: List[int]) -> int:
         """
-
+        Calculate the maximum amount of the water that can be held by the container
 
         Parameters:
-            height (List[int]):
+            height (List[int]): The height of each pillar
 
         Returns:
-            int:
+            int: The maximum amount of water
 
         """
         max_area = 0
@@ -29,8 +29,36 @@ class Solution:
 
         return max_area
 
+    @staticmethod
+    def max_area_two_pointers(height: List[int]) -> int:
+        """
+        Calculate the maximum amount of the water that can be held by the container
+
+        Parameters:
+            height (List[int]): The height of each pillar
+
+        Returns:
+            int: The maximum amount of water
+
+        """
+        left = 0
+        right = len(height) - 1
+        max_area = 0
+
+        while left < right:
+            width = right - left
+            h = min(height[left], height[right])
+            max_area = max(max_area, width * h)
+
+            if height[left] < height[right]:
+                left += 1
+            else:
+                right -= 1
+
+        return max_area
+
 def test():
-    print(Solution.max_area([1, 1]))
+    print(Solution.max_area_two_pointers([1, 1]))
 
 if __name__ == "__main__":
     test()
